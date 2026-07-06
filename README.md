47d6c818c427d637a06d826b64a1f168c1853e68
Maksim Dziatkou <dziatkou.maksim@gmail.com>
2026-07-06 15:03:29 +0200

    feat: add minio cluster and nginx to docker compose

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ```bash
 docker compose up --build
 ```
-FastAPI backend docs: http://localhost:8000/docs
+
+- FastAPI backend docs: http://localhost:8000/docs
+- MinIO WebUI: http://127.0.0.1:9001/
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "ok",
+        "database_url": os.getenv("DATABASE_URL", "Not Set").split("@")[-1], 
+        "minio_endpoint": os.getenv("MINIO_ENDPOINT", "Not Set")
+    }
